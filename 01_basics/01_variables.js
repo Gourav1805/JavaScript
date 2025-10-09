076ec04c5bf34e6a60fecf4fd17312ce6048fe64
Gourav1805 <madaangourav001@gmail.com>
2025-10-09 14:54:47 +0530

    01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,22 @@
 const accountId = 1349
 let accountemail= "madaangourav@gmail.com"
 var accountpassword = "12345"
-accountcity = "patiala"
+accountcity = "patiala" //iske aage to kuch likkha hi nhi - esenhi krna chahiye
+
+let accountstate // undefined h abhi 
 
 
+// accountId = 1234 changing constant is not allowed 
+
+accountemail="garv@gamil.com"
+accountpassword="9876"
+accountcity="noida"
+
+
+/* 
+prefer not to use var- only use const and let 
+beacause of  issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate]);
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello")
+// alert(3+3) coz we are node js not browser
+
+// avoid semicolons -makes it more readable
+
+console.log(3+3)
+console.log("gourav")
+
+
