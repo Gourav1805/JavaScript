d4f44a6a55bfb526932c917c2b3f8d92612a75a9
Gourav1805 <madaangourav001@gmail.com>
2025-10-11 15:51:22 +0530

    03 datatype

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let score =  "33" //true-> 1 //undefined // null //"33abc ->NaN" //becomes string
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isloggedin = ""
+let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin)
+
+//1->true,  0->false,  ""->false ,  "garv"-> true
+
+let somenumber = 33
+let stringsomenumber = String(somenumber)
+
+console.log(stringsomenumber)
+console.log(typeof stringsomenumber)
+
+
+
+// ******************** OPERATIONS **************************
+
+
