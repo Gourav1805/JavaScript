1f0f7f0c940720cf912c8cacb8e4e752e3edfe45
Gourav1805 <madaangourav001@gmail.com>
2025-10-11 15:50:07 +0530

    data types

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -8,4 +8,24 @@
 console.log(3+3)
 console.log("gourav")
 
+let name = "gourav"
+let age = 18
+let isloogedin = false
+let state; 
+
+// number 2 to the power 53
+// bigint 
+//string => ""
+// boolean => true/fasle
+// null -> standalone value
+// undefined -> 
+// symbol -> unique 
+
+// object - 
+
+console.log(typeof "Gourav")
+console.log(typeof 18)
+console.log(typeof "18")
+console.log(typeof null) // interview que
+console.log(typeof undefined)
 
