cd316dc5933aaeecc4ec883f72db2ba0cbbe09c7
Gourav1805 <madaangourav001@gmail.com>
2025-10-12 11:29:24 +0530

    numbers

--- 01_basics/06_num_And_Maths.js
+++ 01_basics/06_num_And_Maths.js
@@ -3,6 +3,12 @@ console.log(score)
 
 const balance = new Number(100)
 console.log(balance)
-const bala = new Number(200)
+const bala = new Number(200.23)
 console.log(bala)
 
+console.log(bala.toString().length)
+console.log(bala.toFixed(1))
+
+
+const other = 123.8966
+console.log(other.toPrecision(3))
