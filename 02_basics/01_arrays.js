ca39926eeb1307b41cd3b746c09dfbd8b1735085
Gourav1805 <madaangourav001@gmail.com>
2025-10-13 12:11:15 +0530

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+// array 
+
+const myArr = [0,1,2,3,4,5]
+const myHeores = ["shaktiman","naagraj"]
+const myArr2 = new Array(1,2,3,4,5)
+
+
+console.log(myArr[0])
+console.log(myArr["one"]) //undefined
+
+//  Array methods bv
