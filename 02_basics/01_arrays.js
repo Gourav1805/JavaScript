9c6cbfca5df8ee163f23daab626080fbc0622e67
Gourav1805 <madaangourav001@gmail.com>
2025-10-13 18:28:54 +0530

    arreys

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,11 +1,44 @@
-// array 
+// array - shallow copy and deep copy 
 
 const myArr = [0,1,2,3,4,5]
 const myHeores = ["shaktiman","naagraj"]
 const myArr2 = new Array(1,2,3,4,5)
 
-
 console.log(myArr[0])
 console.log(myArr["one"]) //undefined
 
-//  Array methods bv
+//  Array methods 
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() //last element uda deta h 
+// console.log(myArr) 
+
+// myArr.unshift(9)  // to add in the front - optimize nih h bs thoda
+// myArr.unshift(0)
+// console.log(myArr)
+// myArr.shift()
+// myArr.shift()
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr) // change of type happened- join binded our array and converted it into a string
+
+
+
+// slice and splice
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+console.log("B ", myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("B ", myArr)
