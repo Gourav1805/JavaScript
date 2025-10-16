33d3dcbea58084d021c7169ad0700f235ceced3d
Gourav1805 <madaangourav001@gmail.com>
2025-10-16 15:14:02 +0530

    key

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,6 +1,7 @@
 // singleton
 
-const { jsx } = require("react/jsx-runtime")
+const mySym = Symbol("key1")
+
 
 // object literals 
 // Object.create  - constructor method - isme singleton bnta h
@@ -11,6 +12,7 @@ const JsUser2 =
     name : "Gourav",
     "full name" : "GOurav MAdaan", // is chiz ko hm dot se access kr hi nhi skte
     age : 20,
+    mySym : "mykey1",
     location : "patiala",
     islogin : false,
     lastLoginDays : ["Monday","Saturday"]
@@ -22,3 +24,17 @@ console.log(JsUser2["location"])
 console.log(JsUser2["lastLoginDays"])
 
 //console.log(JsUser1.full name) // - nhi chlag ye 
+console.log(JsUser2["full name"])
+
+console.log(JsUser2.mySym)
+console.log(typeof JsUser2.mySym) // string aa rha , hme to symbol ki trf use krna hot
+
+console.log(JsUser2["mySym"])
+// console.log(typeof JsUser2["mySym"])
+
+JsUser2.location = "ynr"
+console.log(JsUser2["location"])
+Object.freeze(JsUser2)
+
+JsUser2.location = "india"
+console.log(JsUser2["location"]) // freeze hogya - mtlb ab update nhi hoga to india
